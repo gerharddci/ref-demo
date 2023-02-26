f0667f0b9837f85ffc0854bcb77447925a2e9c17
gnuffer <gfnuffer@gmail.com>
2023-02-26 15:42:59 +0100

    Add ref demo code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [start, setStart] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
+
+  function handleStart() {
+    setStart(Date.now() - (now - start));
+    setNow(Date.now());
+
+    clearInterval(intervalRef.current);
+
+    intervalRef.current = setInterval(function() {
+      setNow(Date.now())
+    }, 100);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+
+  let secondsPassed = 0;
+  if (start !== null && now !== null) {
+    secondsPassed = (now - start) / 1000;
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Number of seconds passed: {secondsPassed}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 }
+
+// function App() {
+//   const inputRef = useRef(null);
+
+//   function handleClick() {
+//     inputRef.current.focus();
+//   }
+
+
+//   return (
+//     <div>
+//       <input ref={inputRef} />
+//       <button onClick={handleClick}>Focus the input</button>
+//     </div>
+//   )
+// }
 
 export default App;
